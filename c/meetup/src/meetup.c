25a80469e03541db1230d3da71c46fe5ff1d479d
Vladimir <jelezarov.vladimir@gmail.com>
2018-02-02 08:31:15 +0100

    WIP meetup

---
+++ c/meetup/src/meetup.c
@@ -0,0 +1,7 @@
+#include "meetup.h"
+
+int meetup_day_of_month(int year, int month, const char *modif,
+			const char *day)
+{
+	return 0;
+}
---
+++ c/meetup/src/meetup.h
@@ -0,0 +1,7 @@
+#ifndef MEETUP_H
+#define MEETUP_H
+
+int meetup_day_of_month(int year, int month, const char *modif,
+			const char *day);
+
+#endif
