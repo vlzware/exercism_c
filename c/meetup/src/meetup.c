0f036d98908a7e6057586566eff947f9e1d95192
Vladimir <jelezarov.vladimir@gmail.com>
2018-02-03 11:33:27 +0100

    meetup

--- c/meetup/src/meetup.c
+++ c/meetup/src/meetup.c
@@ -82,14 +82,11 @@ int get_week_day_num(const char* day)
 	return -1;
 }
 
-/* http://blog.hackerearth.com/2016/11/\
-	algorithm-on-how-to-find-the-day-of-week.html */
-int get_week_day(const int day, const int month, int year)
+/* https://en.wikipedia.org/wiki/Determination_of_the_day_of_the_week\
+	#Implementation-dependent_methods */
+int get_week_day(int d, const int m, int y)
 {
-	int t[] = {0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4};
-	year -= month < 3;
-	return (year + (int)(year/4) - (int)(year/100) + (int)(year/400) +
-		t[month-1] + day) % 7;
+	return (d+=m<3?y--:y-2,23*m/9+d+4+y/4-y/100+y/400)%7;
 }
 
 int get_first_day(const int year, const int month)
