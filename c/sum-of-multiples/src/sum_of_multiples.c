d09e4be2bb1d390882ce1780e44c0f6a2255888e
Vladimir <jelezarov.vladimir@gmail.com>
2018-02-05 18:21:21 +0100

    sum_of_multiples

--- c/sum-of-multiples/src/sum_of_multiples.c
+++ c/sum-of-multiples/src/sum_of_multiples.c
@@ -1,36 +1,28 @@
 #include "sum_of_multiples.h"
 #include <stddef.h>
 
-int alg_sum(int n)
-{
-	return n * (n + 1) / 2;
-}
-
-/* sum of multiples - mathematical approach */
 int sum_of_multiples(const unsigned int *multiples, const int count,
 		    const int n)
 {
-	if (multiples == NULL || count == 0)
+	if (multiples == NULL || count == 0 || !multiples[0])
 		return 0;
 
-	int i = 0, res = 0, multis = 1;
-	for (; i < count; i++) {
-		if (multiples[i] != 0) {
-			res += multiples[i] * alg_sum((n - 1)/multiples[i]);
-			multis *= multiples[i];
-		}
+	/* speedy solution */
+	if (count == 1) {
+		int tmp = (n - 1)/multiples[0];
+		return multiples[0] * tmp * (tmp + 1) /2;
+
 	}
-	if (count > 1)
-		res -= multis * alg_sum((n - 1) / multis);
+
+	/* dumb brute force */
+	int i, k, f, res = 0;
+	for (i = 1; i < n; i++) {
+		f = 0;
+		for (k = 0; k < count; k++)
+			if (!f && multiples[k] && !(i % multiples[k])) {
+				res += i;
+				f = 1;
+			}
+	}
 	return res;
 }
-
-#ifndef PROBLEM_TEST
-#include <stdio.h>
-int main (void)
-{
-	const unsigned int m[] = {4,6};
-	printf("%i\n", sum_of_multiples(m, 2, 15));
-	return 0;
-}
-#endif
