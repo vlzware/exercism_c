3fbc4ded91206f769899b1d11efd4ad314f203a6
Vladimir <jelezarov.vladimir@gmail.com>
2018-02-06 20:19:04 +0100

    atbash_cipher

---
+++ c/atbash-cipher/src/atbash_cipher.c
@@ -0,0 +1,52 @@
+#include "atbash_cipher.h"
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAGIC 219
+
+void addspace(char **tmp, int *count)
+{
+	if (*count == 5) {
+		*count = 0;
+		*(*tmp)++ = ' ';
+	}
+}
+
+/* modes: 0 = decode; 1 = encode */
+char *parse(char *input, int mode)
+{
+	int len = strlen(input);
+	char *tmp = (char*) malloc(len + mode * len/4 + 1);
+	if (tmp == NULL)
+		return NULL;
+
+	char *res = tmp;
+	int count = 0;
+	while (*input) {
+		if (isalpha(*input)) {
+			addspace(&tmp, &count);
+			*tmp = MAGIC - tolower(*input);
+			count += mode;
+			tmp++;
+		} else if (isdigit(*input)) {
+			addspace(&tmp, &count);
+			*tmp = *input;
+			count += mode;
+			tmp++;
+		}
+		input++;
+	}
+	*tmp = '\0';
+	return res;
+}
+
+char *atbash_encode(char *input)
+{
+	return parse(input, 1);
+}
+
+char *atbash_decode(char *input)
+{
+	return parse(input, 0);
+}
---
+++ c/atbash-cipher/src/atbash_cipher.h
@@ -0,0 +1,7 @@
+#ifndef ATBASH_H
+#define ATBASH_H
+
+char *atbash_encode(char *input);
+char *atbash_decode(char *input);
+
+#endif
