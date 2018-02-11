8ac9005e8ac720c36b11260f97add307dadd2d38
Vladimir <jelezarov.vladimir@gmail.com>
2018-02-11 10:39:05 +0100

    triangle

--- c/triangle/src/triangle.c
+++ c/triangle/src/triangle.c
@@ -1,33 +1,44 @@
 #include "triangle.h"
 
-int is_equilateral(triangle_t triangle)
+int check_t(triangle_t t);
+
+int is_equilateral(triangle_t t)
 {
 	if (
-		triangle.a == triangle.b &&
-		triangle.a == triangle.c
+		t.a == t.b &&
+		t.a == t.c
 	)
-		return 1;
+		return check_t(t);
 	return 0;
 }
 
-int is_isosceles(triangle_t triangle)
+int is_isosceles(triangle_t t)
 {
 	if (
-		triangle.a == triangle.b ||
-		triangle.a == triangle.c ||
-		triangle.b == triangle.c
+		t.a == t.b ||
+		t.a == t.c ||
+		t.b == t.c
 	)
-		return 1;
+		return check_t(t);
 	return 0;
 }
 
-int is_scalene(triangle_t triangle)
+int is_scalene(triangle_t t)
 {
 	if (
-		triangle.a != triangle.b &&
-		triangle.a != triangle.c &&
-		triangle.b != triangle.c
+		t.a != t.b &&
+		t.a != t.c &&
+		t.b != t.c
 	)
-		return 1;
+		return check_t(t);
 	return 0;
+}
+
+int check_t(triangle_t t)
+{
+	return
+		(t.a > 0 && t.b > 0 && t.c > 0) &&
+		(t.a < t.b + t.c) &&
+		(t.b < t.a + t.c) &&
+		(t.c < t.a + t.b);
 }
