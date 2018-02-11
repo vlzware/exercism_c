a82bad9187d4fb0a2bf39d6e50d37c8375ed51f2
Vladimir <jelezarov.vladimir@gmail.com>
2018-02-11 09:30:37 +0100

    pascals_triangle

--- c/pascals-triangle/src/pascals_triangle.c
+++ c/pascals-triangle/src/pascals_triangle.c
@@ -1,7 +1,6 @@
 #include "pascals_triangle.h"
 #include <stdlib.h>
 
-int fr(int i);
 void check_alloc(void *p);
 
 size_t **create_triangle(int rows)
@@ -26,9 +25,13 @@ size_t **create_triangle(int rows)
 		triangle[i] = (size_t *) malloc(sizeof(size_t) * cols);
 		check_alloc(triangle[i]);
 
-		for (j = 0; j < cols; j++)
-			triangle[i][j] = (j > i)?
-				0 : fr(i) / (fr(j) * fr(i - j));
+		triangle[i][0] = 1;
+
+		for (j = 1; j < cols; j++)
+			triangle[i][j] = (j > i)
+				? 0
+				: (triangle[i - 1][j] +
+					triangle[i - 1][j - 1]);
 	}
 
 	return triangle;
@@ -43,13 +46,6 @@ void free_triangle(size_t **triangle, int size)
 		free(triangle[size]);
 
 	free(triangle);
-}
-
-int fr(int i)
-{
-	if (i <= 0)
-		return 1;
-	return (i * fr(i - 1));
 }
 
 #include <stdio.h>
